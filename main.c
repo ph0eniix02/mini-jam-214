4779e2c3f4ef958f7cc129bec76d32781eb6aa0d
Aidan Malana <aidanmalana02@gmail.com>
2026-07-04 08:12:36 -0500

    planning doc and readme

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <raylib.h>
 
+#define CORNFLOWER_BLUE (Color) {99, 149, 238, 255} 
 int main(void)
 {
 	printf("Hello World!");
-	InitWindow(320, 180, "Mini Jam 214");
+	InitWindow(960, 540, "Mini Jam 214");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-			ClearBackground(RAYWHITE);
+			ClearBackground(CORNFLOWER_BLUE);
 			DrawText("Hello Raylib!", 0, 0, 20, LIGHTGRAY);
 		EndDrawing();
 	}
--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ gcc -W -Wall -pedantic main.c -I./vendored/raylib/src/ -L./vendored/raylib/src/ 
 # create the compile_commands.json for clangd (just for development)
 bear -- gcc -W -Wall -pedantic main.c -I./vendored/raylib/src/ -L./vendored/raylib/src/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game
 
+# run the game
+./game
